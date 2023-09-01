6db6781a9866a36d53d1dcba648a1ec5a8fe541d
navneet-chugh <navneetchugh15@gmail.com>
2023-09-01 21:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// Get the element with id "level"
+const element = document.getElementById("level");
+
+// Initialize a counter for DOM level
+let domLevel = 0;
+
+// Traverse up the DOM tree to find the DOM level
+let currentElement = element;
+while (currentElement !== document.documentElement) {
+    currentElement = currentElement.parentNode;
+    domLevel++;
+}
+
+// Display the DOM level using alert
+alert(`The level of the element is: ${domLevel}`);
